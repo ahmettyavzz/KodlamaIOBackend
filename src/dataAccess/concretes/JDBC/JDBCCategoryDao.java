525e4a3a022e502d5c855cee54c4ad45e1426701
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-11 20:20:13 +0300

    JDBCCategoryDao

--- src/dataAccess/concretes/JDBC/JDBCCategoryDao.java
+++ src/dataAccess/concretes/JDBC/JDBCCategoryDao.java
@@ -1,4 +1,36 @@
 package dataAccess.concretes.JDBC;
 
-public class JDBCCategoryDao {
+import dataAccess.abstracts.CategoryDao;
+import entity.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCCategoryDao implements CategoryDao {
+
+    List<Category> categories = new ArrayList<>();
+
+
+    @Override
+    public void addCategory(Category category) {
+        System.out.println(category.getCategoryName()+" isimli karegori JDBC ile eklendi.");
+    }
+
+    @Override
+    public void updateCategory(Category category) {   //yeni ismi vs yapılabilir
+        System.out.println(category.getCategoryName()+" isimli karegori JDBC ile güncellendi.");
+
+    }
+
+    @Override
+    public void deleteCategory(Category category) {
+        System.out.println(category.getCategoryName()+" isimli karegori JDBC ile silindi.");
+
+    }
+
+    @Override
+    public List<Category> allCategories() {
+        return categories;
+
+    }
 }
