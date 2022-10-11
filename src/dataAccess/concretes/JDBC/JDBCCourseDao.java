4ed7c2fae1524dc7a4a303ad11aa9fb5b298c533
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-11 20:20:40 +0300

    JDBCCourseDao

--- src/dataAccess/concretes/JDBC/JDBCCourseDao.java
+++ src/dataAccess/concretes/JDBC/JDBCCourseDao.java
@@ -1,4 +1,33 @@
 package dataAccess.concretes.JDBC;
 
-public class JDBCCourseDao {
+import dataAccess.abstracts.CourseDao;
+import entity.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCCourseDao implements CourseDao {
+    List<Course> courses = new ArrayList<>();
+
+    @Override
+    public void addCourse(Course course) {
+        System.out.println(course.getCourseName()+" isimli kurs JDBC ile eklendi.");
+
+    }
+
+    @Override
+    public void updateCourse(Course course) {
+        System.out.println(course.getCourseName()+" isimli kurs JDBC ile güncellendi.");
+    }
+
+    @Override
+    public void deleteCourse(Course course) {
+        System.out.println(course.getCourseName()+" isimli kurs JDBC ile silindi.");
+    }
+
+    @Override
+    public List<Course> allCourses() {
+        return courses;
+    }
+
 }
