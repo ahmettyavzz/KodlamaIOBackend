5ac843fd899d0c2c0bc597501661a585d39430cb
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-11 20:17:15 +0300

    HibernateInstructorDao

--- src/dataAccess/concretes/hibernate/HibernateInstructorDao.java
+++ src/dataAccess/concretes/hibernate/HibernateInstructorDao.java
@@ -1,4 +1,35 @@
 package dataAccess.concretes.hibernate;
 
-public class HibernateInstructorDao {
+import dataAccess.abstracts.InstructorDao;
+import entity.Instructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateInstructorDao implements InstructorDao {
+
+     	List<Instructor> instructors = new ArrayList<>();
+
+    @Override
+    public void addInstructor(Instructor instructor) {
+        System.out.println(instructor.getInstructorName()+" isimli eğitmen hibernate ile eklendi.");
+
+    }
+
+    @Override
+    public void updateInstructor(Instructor instructor) {
+        System.out.println(instructor.getInstructorName()+" isimli eğitmen hibernate ile güncellendi.");
+
+    }
+
+    @Override
+    public void deleteInstructor(Instructor instructor) {
+        System.out.println(instructor.getInstructorName()+" isimli eğitmen hibernate ile silindi.");
+
+    }
+
+    @Override
+    public List<Instructor> allInstructors() {
+        return instructors;
+    }
 }
