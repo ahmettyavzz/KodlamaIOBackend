ff8e59ab4ade3d91fb6be62743ab6fa8a929caf3
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-11 20:21:16 +0300

    JDBCInstructorDao

--- src/dataAccess/concretes/JDBC/JDBCInstructorDao.java
+++ src/dataAccess/concretes/JDBC/JDBCInstructorDao.java
@@ -1,4 +1,35 @@
 package dataAccess.concretes.JDBC;
 
-public class JDBCInstructorDao {
+import dataAccess.abstracts.InstructorDao;
+import entity.Instructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCInstructorDao implements InstructorDao {
+
+    List<Instructor> instructors = new ArrayList<>();
+
+    @Override
+    public void addInstructor(Instructor instructor) {
+        System.out.println(instructor.getInstructorName()+" isimli eğitmen JDBC ile eklendi.");
+
+    }
+
+    @Override
+    public void updateInstructor(Instructor instructor) {
+        System.out.println(instructor.getInstructorName()+" isimli eğitmen JDBC ile güncellendi.");
+
+    }
+
+    @Override
+    public void deleteInstructor(Instructor instructor) {
+        System.out.println(instructor.getInstructorName()+" isimli eğitmen JDBC ile silindi.");
+
+    }
+
+    @Override
+    public List<Instructor> allInstructors() {
+        return instructors;
+    }
 }
