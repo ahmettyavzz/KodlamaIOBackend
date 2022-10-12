ecc8da8723093bb7f1acc0d5bb310247a14ffd3d
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-13 01:35:03 +0300

    JDBCCourseDao.java added data.

--- src/dataAccess/concretes/JDBC/JDBCCourseDao.java
+++ src/dataAccess/concretes/JDBC/JDBCCourseDao.java
@@ -1,7 +1,9 @@
 package dataAccess.concretes.JDBC;
 
 import dataAccess.abstracts.CourseDao;
+import entity.Category;
 import entity.Course;
+import entity.Instructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,17 @@ import java.util.List;
 public class JDBCCourseDao implements CourseDao {
     List<Course> courses = new ArrayList<>();
 
+    public JDBCCourseDao() {
+        Instructor instructor1= new Instructor(1l,"engin");
+
+
+        Category category1=new Category(1l,"Java",null);
+        Category category2=new Category(1l,"C#",null);
+        Course course3 = new Course(1l,".Net",instructor1,category2,12.75);
+        Course course4 = new Course(1l,"Angular",instructor1,category2,-2);
+        courses.add(course3);
+        courses.add(course4);
+    }
     @Override
     public void addCourse(Course course) {
         System.out.println(course.getCourseName()+" isimli kurs JDBC ile eklendi.");
