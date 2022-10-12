65d1e6e353cf05e586b4b019f510217d7aa7a2f5
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-13 01:34:13 +0300

    HibernateCourseDao.java added data.

--- src/dataAccess/concretes/hibernate/HibernateCourseDao.java
+++ src/dataAccess/concretes/hibernate/HibernateCourseDao.java
@@ -1,14 +1,30 @@
 package dataAccess.concretes.hibernate;
 
 import dataAccess.abstracts.CourseDao;
+import entity.Category;
 import entity.Course;
+import entity.Instructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HibernateCourseDao implements CourseDao {
 
+
+
     List<Course> courses = new ArrayList<>();
+
+    public HibernateCourseDao() {
+        Instructor instructor1= new Instructor(1l,"engin");
+
+
+        Category category1=new Category(1l,"Java",null);
+        Category category2=new Category(1l,"C#",null);
+        Course course3 = new Course(1l,".Net",instructor1,category2,12.75);
+        Course course4 = new Course(1l,"Angular",instructor1,category2,-2);
+        courses.add(course3);
+        courses.add(course4);
+    }
 
     @Override
     public void addCourse(Course course) {
