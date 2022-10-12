64f9cb8440a22045e08b035c72c3ad52299a3946
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-12 15:04:37 +0300

    CategoryService and CategoryManager

--- src/business/abstracts/CategoryService.java
+++ src/business/abstracts/CategoryService.java
@@ -1,4 +1,14 @@
 package business.abstracts;
 
+import entity.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    void addCategory(Category category);
+    void updateCategory(Category category);
+
+    void deleteCategory(Category category);
+
+    List<Category> allCategories();
 }
--- src/business/concretes/CategoryManager.java
+++ src/business/concretes/CategoryManager.java
@@ -1,4 +1,38 @@
 package business.concretes;
 
-public class CategoryManager {
+import business.abstracts.CategoryService;
+import dataAccess.abstracts.CategoryDao;
+import entity.Category;
+
+import java.util.List;
+
+public class CategoryManager implements CategoryService {
+
+    private CategoryDao categoryDao;
+    private List<Category> categories;
+
+
+    public CategoryManager(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @Override
+    public void addCategory(Category category) {
+        categoryDao.addCategory(category);
+    }
+
+    @Override
+    public void updateCategory(Category category) {
+        categoryDao.updateCategory(category);
+    }
+
+    @Override
+    public void deleteCategory(Category category) {
+        categoryDao.deleteCategory(category);
+    }
+
+    @Override
+    public List<Category> allCategories() {
+        return categories;
+    }
 }
