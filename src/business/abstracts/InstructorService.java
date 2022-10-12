2907a10c0eef7eecccd88a47644ffe331c8f9db1
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-12 15:03:46 +0300

    InstructorService and InstructorManager

--- src/business/abstracts/InstructorService.java
+++ src/business/abstracts/InstructorService.java
@@ -1,4 +1,15 @@
 package business.abstracts;
 
+import entity.Course;
+import entity.Instructor;
+
+import java.util.List;
+
 public interface InstructorService {
+    void addInstructor(Instructor instructor);
+    void updateInstructor(Instructor instructor);
+
+    void deleteInstructor(Instructor instructor);
+
+    List<Instructor> allInstructors();
 }
--- src/business/concretes/InstructorManager.java
+++ src/business/concretes/InstructorManager.java
@@ -1,4 +1,38 @@
 package business.concretes;
 
-public class InstructorManager {
+import business.abstracts.InstructorService;
+import dataAccess.abstracts.InstructorDao;
+import entity.Course;
+import entity.Instructor;
+
+import java.util.List;
+
+public class InstructorManager implements InstructorService {
+
+    private InstructorDao instructorDao;
+    private List<Instructor> instructors;
+
+    public InstructorManager(InstructorDao instructorDao) {
+        this.instructorDao = instructorDao;
+    }
+
+    @Override
+    public void addInstructor(Instructor instructor) {
+        instructorDao.addInstructor(instructor);
+    }
+
+    @Override
+    public void updateInstructor(Instructor instructor) {
+        instructorDao.updateInstructor(instructor);
+    }
+
+    @Override
+    public void deleteInstructor(Instructor instructor) {
+        instructorDao.deleteInstructor(instructor);
+    }
+
+    @Override
+    public List<Instructor> allInstructors() {
+        return instructors;
+    }
 }
