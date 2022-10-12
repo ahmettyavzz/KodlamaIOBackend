2c3ce2f5aebd72ba725170647fdbc2670c27f565
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-13 01:34:48 +0300

    JDBCInstructorDao.java added data.

--- src/dataAccess/concretes/JDBC/JDBCInstructorDao.java
+++ src/dataAccess/concretes/JDBC/JDBCInstructorDao.java
@@ -10,6 +10,11 @@ public class JDBCInstructorDao implements InstructorDao {
 
     List<Instructor> instructors = new ArrayList<>();
 
+
+    public JDBCInstructorDao() {
+        Instructor instructor1= new Instructor(1l,"engin");
+        instructors.add(instructor1);
+    }
     @Override
     public void addInstructor(Instructor instructor) {
         System.out.println(instructor.getInstructorName()+" isimli eğitmen JDBC ile eklendi.");
